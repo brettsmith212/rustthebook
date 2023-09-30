fe6fe6220fd0a197051a4abc14811c3eeec03d29
Brett Smith <67445684+brettsmith212@users.noreply.github.com>
2023-09-29 23:18:18 -0700

    Delete target directory

--- target/debug/deps/rust_the_book-b2d4410d6a31c73b.d
+++
@@ -1,5 +0,0 @@
-/home/runner/rustthebook/target/debug/deps/rust_the_book-b2d4410d6a31c73b: projects/guessing_game/main.rs
-
-/home/runner/rustthebook/target/debug/deps/rust_the_book-b2d4410d6a31c73b.d: projects/guessing_game/main.rs
-
-projects/guessing_game/main.rs:
--- target/debug/rust-the-book.d
+++
@@ -1,1 +0,0 @@
-/home/runner/rustthebook/target/debug/rust-the-book: /home/runner/rustthebook/projects/guessing_game/main.rs
