4da872648c2407ea92c1c96e69165bce2816d37e
Brett Smith <brettsmith212@gmail.com>
2023-10-02 22:28:03 -0700

    collections vectors

---
+++ projects/collections/main.rs
@@ -0,0 +1,27 @@
+#[derive(Debug)]
+enum SpreadsheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
+}
+fn main() {
+    let v = vec![1, 2, 3];
+    let mut w = Vec::new();
+    w.push(5);
+    w.push(6);
+    w.push(7);
+
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(third) => println!("3rd element is {}", third),
+        None => println!("There is no third element"),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+
+    println!("row: {:?}", row);
+}
