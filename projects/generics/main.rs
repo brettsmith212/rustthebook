1d65b3fd10dddb0191930ba938c5cb26907511ef
Brett Smith <brettsmith212@gmail.com>
2023-10-07 11:22:24 -0700

    traits 1

--- projects/generics/main.rs
+++ projects/generics/main.rs
@@ -1,22 +1,36 @@
-fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
-    let mut largest = &list[0];
+// ** Generics ** //
+// fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+//     let mut largest = &list[0];
 
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-    largest
-}
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//     largest
+// }
 
+// fn main() {
+//     let numbers_list = vec![34, 50, 25, 100, 65];
+
+//     let result = largest(&numbers_list);
+//     println!("The largest number is {}", result);
+
+//     let char_list = vec!['y', 'm', 'a', 'q'];
+
+//     let result = largest(&char_list);
+//     println!("The largest char is {}", result);
+// }
+
+// ** Traits ** //
+use aggregator::{Summary, Tweet};
 fn main() {
-    let numbers_list = vec![34, 50, 25, 100, 65];
+    let tweet = Tweet {
+        username: String::from("Glen"),
+        content: String::from("yo whaddup dog this is your boy gl3n!"),
+        reply: false,
+        retweet: false,
+    };
 
-    let result = largest(&numbers_list);
-    println!("The largest number is {}", result);
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest char is {}", result);
+    println!("1 new tweet: {}", tweet.summarize());
 }
